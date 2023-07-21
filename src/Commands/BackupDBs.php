9255ab997908d48afee67dd839b6c3482c056105
Alex Gh <a-l-e-x_1379@hotmail.com>
2023-07-21 15:17:08 -0600

    cambiamos nombre de clase

--- src/Commands/BackupDBs.php
+++ src/Commands/BackupDBs.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB as DBs;
 
-class BackupDB extends Command
+class BackupDBs extends Command
 {
     /**
      * The name and signature of the console command.
