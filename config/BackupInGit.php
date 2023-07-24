f164131844b6e4642972b521c762436a2badab4b
Alex Gh <a-l-e-x_1379@hotmail.com>
2023-07-24 12:15:12 -0600

    Udate: archivo de configuracion
    Agregamos parametros para actualizacion de bases de datos anidadas

--- config/BackupInGit.php
+++ config/BackupInGit.php
@@ -2,72 +2,108 @@
 
 return [
 
-    /*
+	/*
     |--------------------------------------------------------------------------
-    | Tabla central
-    |--------------------------------------------------------------------------
-    |
-    | Este valor definr el nombre de la tabla la cual contiene todos los accesos
-    | a las demas bases de datos.
-    |
-    */
-
-    'table_center' => 'clients',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Columna de Servidor
+    | Carpeta contenedora
     |--------------------------------------------------------------------------
     |
-    | Este valor define la columna corresponde al servidor dentro
-    | de la tabla central (table_center)
+    | Este valor definr el nombre de la carpeta la cual almacenara los
+    | respaldos y el repositorio.
     |
     */
 
-    'col_server' => 'server',
+    'folder_backup' => 'backup/',
 
-    /*
+	/*
     |--------------------------------------------------------------------------
-    | Columna de base datos
+    | Bases anidadas
     |--------------------------------------------------------------------------
     |
-    | Este valor define la columna corresponde al nombre de la base de datos
-    | dentro de la tabla central (table_center)
-    |
-    */
-
-    'col_db' => 'database',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Columna de Contraseña
-    |--------------------------------------------------------------------------
-    |
-    | Este valor define la columna corresponde a la contraseña dentro de la
-    | tabla central (table_center)
+    | Este valor activa o desactiva el respaldo de bases anidadas, la cual
+    | debe tener el acceso a las demas base de datos
     |
     */
 
-    'col_pwd' => 'password',
+    'nested_databases_status' => false,
 
-    /*
+	/*
     |--------------------------------------------------------------------------
-    | Estatus activo de fila
+    | Valores de Bases anidadas
     |--------------------------------------------------------------------------
     |
-    | Este valor define la columna corresponde al status de la fila, ademas
-	| de definir el valor activo de la columna, esto con el objetivo de no
-	| realizar respados a las bases eliminadas o inactivas.
-	|
-	| Si al valor status.status_enable esta en false, se da por echo que
-	| todas las filas son activas y respaldara todo.
+    | Este array contiene los parametros de las bases de datos anidadas.
+    | Debe existir una tabla con los accesos las demas bases de datos.
     |
     */
 
-    'status' => [
-		'status_enable' => true, // true o false
-		'col_status' => 'status', // string del nombre de columna
-		'value_active' => 1, // Valor que define estado activo
-	],
+	'nested_databases' => [
+		/*
+		|--------------------------------------------------------------------------
+		| Tabla central
+		|--------------------------------------------------------------------------
+		|
+		| Este valor definr el nombre de la tabla la cual contiene todos los accesos
+		| a las demas bases de datos.
+		|
+		*/
 
+		'table_center' => 'clients',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Columna de Servidor
+		|--------------------------------------------------------------------------
+		|
+		| Este valor define la columna corresponde al servidor dentro
+		| de la tabla central (table_center)
+		|
+		*/
+
+		'col_server' => 'server',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Columna de base datos
+		|--------------------------------------------------------------------------
+		|
+		| Este valor define la columna corresponde al nombre de la base de datos
+		| dentro de la tabla central (table_center)
+		|
+		*/
+
+		'col_db' => 'database',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Columna de Contraseña
+		|--------------------------------------------------------------------------
+		|
+		| Este valor define la columna corresponde a la contraseña dentro de la
+		| tabla central (table_center)
+		|
+		*/
+
+		'col_pwd' => 'password',
+
+		/*
+		|--------------------------------------------------------------------------
+		| Estatus activo de fila
+		|--------------------------------------------------------------------------
+		|
+		| Este valor define la columna corresponde al status de la fila, ademas
+		| de definir el valor activo de la columna, esto con el objetivo de no
+		| realizar respados a las bases eliminadas o inactivas.
+		|
+		| Si al valor status.status_enable esta en false, se da por echo que
+		| todas las filas son activas y respaldara todo.
+		|
+		*/
+
+		'status' => [
+			'status_enable' => true, // true o false
+			'col_status' => 'status', // string del nombre de columna
+			'value_active' => 1, // Valor que define estado activo
+		],
+
+	]
 ];
